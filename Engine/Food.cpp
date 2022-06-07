c1a89fc0786fb4d0ff1a2e8b0242ce1f0601b2ae
Hermes <hermeschigoziri@gmail.com>
2022-06-07 22:24:42 +0100

    Delegate logic for respawning food object to the Food class

--- Engine/Food.cpp
+++ Engine/Food.cpp
@@ -1,8 +1,11 @@
 #include "Food.h"
 
-Food::Food(const Location& _cordinate)
+Food::Food()
+	:random_num_generator(std::random_device()()),
+	xDist(0, 38),
+	yDist(0, 28)
 {
-	cordinate = _cordinate;
+	cordinate = { xDist(random_num_generator), yDist(random_num_generator) };
 }
 
 void Food::draw(Board& board)
@@ -10,9 +13,9 @@ void Food::draw(Board& board)
 	board.drawCell(cordinate, Colors::Gray);
 }
 
-void Food::setCordinate(const Location& _cordinate)
+void Food::respawn()
 {
-	cordinate = _cordinate;
+	cordinate = { xDist(random_num_generator), yDist(random_num_generator) };
 }
 
 bool Food::isEaten(const Snake& player)
